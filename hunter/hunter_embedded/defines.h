3a5070a5167bab82e9b68f826c44294dc8a4eb78
f-crivellaro <f-crivellaro@fct.unl.pt>
2022-06-10 01:46:25 +0100

    Frequency controlling working

--- hunter/hunter_embedded/defines.h
+++ hunter/hunter_embedded/defines.h
@@ -4,4 +4,14 @@
 #define WIFI_SSID         "NOS-ECD6"
 #define WIFI_PASSWORD     "ZFJNW2VQ"
 
+#define RESET             17
+#define DRV_EN            26
+#define DRV_A0            21
+#define DRV_A1            4
+#define RX_EN             32
+#define RX_A0             33
+#define RX_A1             25
+
+#define DDS_MCLK          50000000
+
 #endif    //defines_h
--- hunter/hunter_embedded/hunter_embedded.ino
+++ hunter/hunter_embedded/hunter_embedded.ino
@@ -1,4 +1,5 @@
 #include "EspMQTTClient.h"
+#include <stdio.h>
 #include <string.h>
 #include "defines.h"
 #include <SPI.h>
@@ -12,8 +13,8 @@ SPIClass * hspi = NULL;
 EspMQTTClient client(
   WIFI_SSID,
   WIFI_PASSWORD,
-  "192.168.1.4",  // MQTT Broker server ip
-  "TestClient",     // Client name that uniquely identify your device
+  "192.168.1.254",  // MQTT Broker server ip
+  "CCCRiveLlAro",     // Client name that uniquely identify your device
   1883              // The MQTT port, default to 1883. this line can be omitted
 );
 
@@ -37,7 +38,7 @@ void SPI_Init(void)
   //pinMode(hspi->pinSS(), OUTPUT); //HSPI SS
 }
 
-void SPI_Write(word data)
+void SPI_Write(word data) 
 {
   vspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE2));
   digitalWrite(vspi->pinSS(), LOW); // begin the transfer
@@ -54,21 +55,29 @@ void SPI_Write(word data)
 
 void setup()
 {
-  pinMode(17, OUTPUT); // Reset
+  pinMode(RESET, OUTPUT);   // Reset
+  pinMode(DRV_EN, OUTPUT);  // DRV_EN
+  pinMode(DRV_A0, OUTPUT);  // DRV_A0
+  pinMode(DRV_A1, OUTPUT);  // DRV_A1
+  pinMode(RX_EN, OUTPUT);   // RX_EN
+  pinMode(RX_A0, OUTPUT);   // RX_A0
+  pinMode(RX_A1, OUTPUT);   // RX_A1
+  digitalWrite(RESET, LOW);  
+  digitalWrite(DRV_EN, LOW);  
+  digitalWrite(DRV_A0, LOW);  
+  digitalWrite(DRV_A1, LOW);  
+  digitalWrite(RX_EN, LOW);  
+  digitalWrite(RX_A0, LOW);  
+  digitalWrite(RX_A1, LOW);  
+  
   Serial.begin(115200);
 
   // Optional functionalities of EspMQTTClient
   client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
   //client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
   //client.enableOTA(); // Enable OTA (Over The Air) updates. Password defaults to MQTTPassword. Port is the default OTA port. Can be overridden with enableOTA("password", port).
-  client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
-
-  static const int spiClk = 1000000; // 1 MHz
-  //uninitalised pointers to SPI objects
-  SPIClass * vspi = NULL;
-  SPIClass * hspi = NULL;
+  //client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to tru
   
-
   Serial.println("Resetting DDS..");
   digitalWrite(17, LOW);
   delay(500);
@@ -77,25 +86,124 @@ void setup()
   digitalWrite(17, HIGH);
   Serial.println("Reset High");
   delay(2000);
+  SPI_Write(0x2200);
   digitalWrite(17, LOW);
   Serial.println("Reset Low");
   delay(2000);
-  SPI_Write(0x2200);
   Serial.println("Setting Frequency..");
   //SPI_Write(0x54E6); // 1000 Hz
-  SPI_Write(0x4A8C); //500 Hz
-  SPI_Write(0x4000);
+  //SPI_Write(0x4A7C); //500 Hz
+  //SPI_Write(0x4000);
+  SPI_Write(30147); //500 kHz
+  SPI_Write(16547);
   Serial.println("All Setup Done!");
   
 }
 
-//void setTxGain(int gain) {
-//  Serial.println("TX Gain Setting of: %i", gain);
-//}
-//
-//void setRxGain(int gain) {
-//  Serial.println("RX Gain Setting of: %i", gain);
-//}
+void setTxGain(char *gain) {
+  char tmp[30];
+  if (!strncmp("00", gain, 2)) {
+    sprintf(tmp, "TX Gain Setting of: %s", gain);
+    Serial.println(tmp);
+    digitalWrite(DRV_EN, LOW);  
+  } else if (!strncmp("11", gain, 2)) {
+    sprintf(tmp, "TX Gain Setting of: %s", gain);
+    Serial.println(tmp);
+    digitalWrite(DRV_EN, HIGH);
+    digitalWrite(DRV_A0, LOW);
+    digitalWrite(DRV_A1, LOW);
+  } else if (!strncmp("05", gain, 2)) {
+    sprintf(tmp, "TX Gain Setting of: %s", gain);
+    Serial.println(tmp);
+    digitalWrite(DRV_EN, HIGH);
+    digitalWrite(DRV_A0, HIGH);
+    digitalWrite(DRV_A1, LOW);
+  } else if (!strncmp("03", gain, 2)) {
+    sprintf(tmp, "TX Gain Setting of: %s", gain);
+    Serial.println(tmp);
+    digitalWrite(DRV_EN, HIGH);
+    digitalWrite(DRV_A0, LOW);
+    digitalWrite(DRV_A1, HIGH);
+  } else if (!strncmp("02", gain, 2)) {
+    sprintf(tmp, "TX Gain Setting of: %s", gain);
+    Serial.println(tmp);
+    digitalWrite(DRV_EN, HIGH);
+    digitalWrite(DRV_A0, HIGH);
+    digitalWrite(DRV_A1, HIGH);
+  }
+  
+}
+
+void setRxGain(char *gain) {
+  char tmp[30];
+  if (!strncmp("00", gain, 2)) {
+    sprintf(tmp, "RX Gain Setting of: %s", gain);
+    Serial.println(tmp);
+    digitalWrite(RX_EN, LOW);  
+  } else if (!strncmp("11", gain, 2)) {
+    sprintf(tmp, "RX Gain Setting of: %s", gain);
+    Serial.println(tmp);
+    digitalWrite(RX_EN, HIGH);
+    digitalWrite(RX_A0, LOW);
+    digitalWrite(RX_A1, LOW);
+  } else if (!strncmp("05", gain, 2)) {
+    sprintf(tmp, "RX Gain Setting of: %s", gain);
+    Serial.println(tmp);
+    digitalWrite(RX_EN, HIGH);
+    digitalWrite(RX_A0, HIGH);
+    digitalWrite(RX_A1, LOW);
+  } else if (!strncmp("03", gain, 2)) {
+    sprintf(tmp, "RX Gain Setting of: %s", gain);
+    Serial.println(tmp);
+    digitalWrite(RX_EN, HIGH);
+    digitalWrite(RX_A0, LOW);
+    digitalWrite(RX_A1, HIGH);
+  } else if (!strncmp("02", gain, 2)) {
+    sprintf(tmp, "RX Gain Setting of: %s", gain);
+    Serial.println(tmp);
+    digitalWrite(RX_EN, HIGH);
+    digitalWrite(RX_A0, HIGH);
+    digitalWrite(RX_A1, HIGH);
+  }
+}
+
+
+void setFreq(char *freqout) {
+  char tmp[30];
+  sprintf(tmp, "Frequency Setting of: %s", freqout);
+  Serial.println(tmp);
+  float freqout_float = atof(freqout);
+  Serial.println(freqout_float);
+  int freqreg = round(freqout_float/(DDS_MCLK/pow(2,28)));
+  Serial.println(freqreg);
+  Serial.println("Setting Frequency FREQ0");
+  word freqreg_low = (freqreg & 0x3FFF) | 0x4000;
+  word freqreg_high = (freqreg >> 14) | 0x4000;
+  Serial.println(freqreg_low);
+  Serial.println(freqreg_high);
+
+//  Serial.println("Resetting DDS..");
+//  digitalWrite(17, LOW);
+//  delay(500);
+//  SPI_Write(0x2300);
+//  digitalWrite(17, HIGH);
+//  Serial.println("Reset High");
+//  delay(2000);
+//  SPI_Write(0x2200);
+//  digitalWrite(17, LOW);
+//  Serial.println("Reset Low");
+  Serial.println("Resetting");
+  SPI_Write(0x2100);
+  delay(500);
+  Serial.println("Setting Frequency..");
+  SPI_Write(freqreg_low);
+  SPI_Write(freqreg_high);
+//  SPI_Write(21753); 
+//  SPI_Write(16384);
+  SPI_Write(0x2000);
+  Serial.println("Ok");
+}
+
 
 void MQTTSubCallback(const String topic, const String payload) {
     char tmp[50];
@@ -106,12 +214,18 @@ void MQTTSubCallback(const String topic, const String payload) {
     if (!strncmp("tx", token, 2)) {
       Serial.println("Received a TX Gain configuration");
       token = strtok(NULL, "-");
-      //setTxGain(atoi(token));
+      Serial.println(token);
+      setTxGain(token);
     } else if (!strncmp("rx", token, 2)) {
       Serial.println("Received a RX Gain configuration");
       token = strtok(NULL, "-");
-      Serial.println(atoi(token));
-      //setRxGain(atoi(token));      
+      Serial.println(token);
+      setRxGain(token);      
+    } else if (!strncmp("fr", token, 2)) {
+      Serial.println("Received a Frequency configuration");
+      token = strtok(NULL, "-");
+      Serial.println(token);
+      setFreq(token);
     }
     client.publish("hunter/reply", "Done!!");
 }
